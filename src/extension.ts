17261a2ec7a6972470fdbc609c50cf2ae8116285
Jesus Briales <jesusbriales@gmail.com>
2022-09-27 20:22:02 +0200

    Show ticket in status bar while Debug-mode is active

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,19 @@
 import * as vscode from 'vscode';
 
+const statusBarItem = vscode.window.createStatusBarItem(
+	'debugMode',
+	vscode.StatusBarAlignment.Left,
+	Number.MIN_SAFE_INTEGER // Furthest right on the left
+);
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('The extension "debug-mode" is now active!');
+
+	// Define item for status bar to indicate the mode is active
+	statusBarItem.name = 'Debug Mode status';
+	statusBarItem.text = 'DebugMode';
+	statusBarItem.tooltip = 'GDB-inspired alpha-keys will trigger Debug actions (check for "debugMode.active" in keybindings)';
+	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground');
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('debugMode.toggle', async () => {
@@ -10,10 +22,15 @@ export function activate(context: vscode.ExtensionContext) {
 			isEnabled = !isEnabled;
 			context.workspaceState.update("debugMode.enabled", isEnabled);
 
+			if (isEnabled) {
+				statusBarItem.show();
+			}
+			else {
+				statusBarItem.hide();
+			}
+
 			// Set flag in the Context so that we can use within when-clauses
 			await vscode.commands.executeCommand('setContext', 'debugMode.active', isEnabled);
-
-			vscode.window.showInformationMessage('Debug Mode: ' + (isEnabled ? "Enabled" : "Disabled"));
 		})
 	);
 }
